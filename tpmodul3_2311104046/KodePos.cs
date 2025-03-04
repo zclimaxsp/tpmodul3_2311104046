3709fedc2f336b34d86488071ae8c5cad6ebf7a3
zclimaxsp <dhiemasikhsan@gmail.com>
2025-03-04 09:59:03 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul3_2311104046/KodePos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace tpmodul3_2311104046
+{
+    public class KodePos
+    {
+        // Menggunakan Dictionary untuk teknik Table-Driven
+        private static readonly Dictionary<string, string> kodePosMap = new Dictionary<string, string>
+    {
+        { "Batununggal", "40266" },
+        { "Kujangsari", "40287" },
+        { "Mengger", "40267" },
+        { "Wates", "40256" },
+        { "Cijagra", "40287" },
+        { "Jatisari", "40286" },
+        { "Margasari", "40286" },
+        { "Sekejati", "40286" },
+        { "Kebonwaru", "40272" },
+        { "Maleer", "40274" },
+        { "Samoja", "40273" }
+    };
+
+        // Method untuk mendapatkan kode pos berdasarkan nama kelurahan
+        public static string GetKodePos(string kelurahan)
+        {
+            if (kodePosMap.TryGetValue(kelurahan, out string kodePos))
+            {
+                return kodePos;
+            }
+            else
+            {
+                return "Kode Pos tidak ditemukan";
+            }
+        }
+    }
+}
--- tpmodul3_2311104046/Program.cs
+++ tpmodul3_2311104046/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmodul3_2311104046;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Pencarian Kode Pos ===");
+
+        while (true)
+        {
+            Console.Write("Masukkan nama kelurahan (atau ketik 'exit' untuk keluar): ");
+            string kelurahan = Console.ReadLine();
+
+            if (kelurahan.ToLower() == "exit")
+            {
+                break;
+            }
+
+            string kodePos = KodePos.GetKodePos(kelurahan);
+            Console.WriteLine($"Kode Pos {kelurahan}: {kodePos}\n");
+        }
+
+        Console.WriteLine("Program selesai.");
+    }
+}
